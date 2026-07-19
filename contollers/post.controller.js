7b4071d199244dd5d880884795d97e81660eadeb
Yash <yash.ash12629@gmail.com>
2026-07-20 00:08:34 +0530

    started with post controller

--- contollers/post.controller.js
+++ contollers/post.controller.js
@@ -1,8 +1,12 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js"
-import User from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import ApiResponse from "../utils/ApiResponse.js"
-import jwt from "jsonwebtoken"
 
-const createPost=req.user()
+
+const post=asyncHandler(async(req,res)=>{
+    const title=req.body?.title?.trim()
+    const content=req.body?.content?.trim()
+
+
+})
